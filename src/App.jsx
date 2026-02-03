082c15cc4694e885dddd19a56db37f18c2266519
Norbu Sherpa <norbu.sherpa@firstfintech.com>
2026-02-03 09:16:21 +0545

    calling my first api using fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoLists from "./components/TodoLists";
 import AddTodo from "./components/AddTodo";
 
-const initialTodoLists = [
-  { title: "Study", description: "Read React docs", completed: false },
-  { title: "Exercise", description: "Go for a run", completed: false },
-  { title: "Grocery Shopping", description: "Buy vegetables", completed: false },
-];
+// const initialTodoLists = [
+//   { title: "Study", description: "Read React docs", completed: false },
+//   { title: "Exercise", description: "Go for a run", completed: false },
+//   {
+//     title: "Grocery Shopping",
+//     description: "Buy vegetables",
+//     completed: false,
+//   },
+// ];
 
 function App() {
-  const [todoLists, setTodoLists] = useState(initialTodoLists);
+  const [todoLists, setTodoLists] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => setTodoLists(data));
+  }, []);
 
   return (
     <>
