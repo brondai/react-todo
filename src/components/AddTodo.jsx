23b32efee5cc1b3b3d8b064bb5f02f3881fb17ec
Norbu Sherpa <norbu.sherpa@firstfintech.com>
2026-02-03 08:44:48 +0545

    adding logic implented in form instead of button onclick

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -7,8 +7,18 @@ const AddTodo = ({ setTodoLists, todoLists }) => {
     completed: false,
   });
 
+  const handleChange = (e, name) =>
+    setTodo({ ...todo, [name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodoLists([...todoLists, todo]);
+    setTodo({ title: "", description: "", completed: false });
+  };
+
   return (
-    <form onSubmit={() => {}}>
+    <form onSubmit={handleSubmit}>
       <div className="max-w-md mx-auto mt-8 bg-white p-6 rounded-lg gap-3">
         <h2 className="text-xl font-bold text-blue-700 mb-2">Add a New Todo</h2>
         <input
@@ -16,28 +26,18 @@ const AddTodo = ({ setTodoLists, todoLists }) => {
           className="border border-blue-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-200"
           placeholder="Todo title (required)"
           value={todo.title}
-          onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+          onChange={(e) => handleChange(e, "title")}
         />
         <input
           type="text"
           className="border border-gray-300 p-2 rounded focus:outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Description (optional)"
           value={todo.description}
-          onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+          onChange={(e) => handleChange(e, "description")}
         />
         <button
           className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded transition disabled:bg-blue-200 disabled:cursor-not-allowed"
-          onClick={() => {
-            setTodoLists([
-              ...todoLists,
-              {
-                title: todo.title,
-                description: todo.description,
-                completed: false,
-              },
-            ]);
-            setTodo({ title: "", description: "", completed: false });
-          }}
+          type="submit"
         >
           Add Todo
         </button>
